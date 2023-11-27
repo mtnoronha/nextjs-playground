62b6535440bcb60e1dfde48ff4c4a03711d1d1ef
Marco Noronha <mtnoronha7@gmail.com>
2023-11-27 10:00:05 -0300

    Add about page and param page as examples

---
+++ app/param/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = ({ params }: { params: { id: string }}) => {
+  return (
+    <div>
+      <h1>This page receives params!</h1>
+      <p>Check it out `{ params.id }` </p>
+    </div>
+  );
+}
+
+export default Page;
